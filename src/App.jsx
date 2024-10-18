1f42c1889f4142b8a00d350a18d62d2b6db2a4e7
Dennis <draymontford@gmail.com>
2024-10-18 12:07:15 -0500

    insert

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CoffeeForm from "./components/CoffeeForm"
+import Hero from "./components/Hero"
+import History from "./components/History"
+import Layout from "./components/Layout"
+import Stats from "./components/Stats"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+  const isAuthenticated = true
+  
+  const authenticatedContent = (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Stats />
+      <History />
     </>
+  )
+ 
+  return (
+    <Layout>
+      <Hero />
+      <CoffeeForm />
+      {isAuthenticated && (authenticatedContent)}
+    </Layout>
   )
 }
 
